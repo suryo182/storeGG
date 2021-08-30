1ebfb2e47359fa0f7da03e0c128d3eb027d409be
Suryo Adi Nugroho <40680835+suryo182@users.noreply.github.com>
2021-08-30 22:37:52 +0700

    Convert html pages to nextjs
    - Detail Page
    - Checkout Page
    - Completed Page
    - Sign In Page
    - Sign Up Page
    - Sign Up Phone Page
    - 404 Page
    - Member Overview, Transactions, Transactions Detail
    - Member Edit Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
 import '../styles/homepage.css';
 import '../styles/utilities.css';
+import '../styles/detail.css';
+import '../styles/checkout.css';
+import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
+import '../styles/sign-up-success.css';
+import '../styles/404-not-found.css';
+import '../styles/overview.css';
+import '../styles/sidebar.css';
+import '../styles/transactions.css';
+import '../styles/edit-profile.css';
 
 import { useEffect } from 'react';
 import Head from 'next/head';
