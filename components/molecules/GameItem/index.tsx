dae2fc7beb716bc455502f9c7610dfe8ab72cb1c
Suryo Adi Nugroho <40680835+suryo182@users.noreply.github.com>
2021-09-01 23:34:31 +0700

    detail game WIP

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface GameItemProps {
   title: string;
@@ -13,33 +14,35 @@ interface GameItemProps {
 export default function GameItem({ title, type, thumbnail }: GameItemProps) {
   return (
     <div className="featured-game-card position-relative">
-      <a href="./src/detail.html">
-        <div className="blur-sharp">
-          <Image
-            className="thumbnail"
-            src={`/img/${thumbnail}.png`}
-            width={205}
-            height={270}
-            alt=""
-          />
-        </div>
-        <div className="cover position-absolute bottom-0 m-32">
-          <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
-            <div className="game-icon mx-auto">
-              <Image
-                src="/icon/controller.svg"
-                width={54}
-                height={36}
-                alt="controller"
-              />
-            </div>
-            <div>
-              <p className="fw-semibold text-white text-xl m-0">{title}</p>
-              <p className="fw-light text-white m-0">{type}</p>
+      <Link href="/detail">
+        <a>
+          <div className="blur-sharp">
+            <Image
+              className="thumbnail"
+              src={`/img/${thumbnail}.png`}
+              width={205}
+              height={270}
+              alt=""
+            />
+          </div>
+          <div className="cover position-absolute bottom-0 m-32">
+            <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
+              <div className="game-icon mx-auto">
+                <Image
+                  src="/icon/controller.svg"
+                  width={54}
+                  height={36}
+                  alt="controller"
+                />
+              </div>
+              <div>
+                <p className="fw-semibold text-white text-xl m-0">{title}</p>
+                <p className="fw-light text-white m-0">{type}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </a>
+        </a>
+      </Link>
     </div>
   );
 }
